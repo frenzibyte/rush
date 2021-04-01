eb91b1b5a37e5e07c7a30f0f33b3ea8751b59f2e
Salman Ahmed <frenzibyte@gmail.com>
2021-04-01 15:02:50 +0300

    Move rush! resources to own library project

---
+++ Rush.Resources/RushResource.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Shane Woolcock. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Reflection;
+
+namespace Rush.Resources
+{
+    public static class RushResource
+    {
+        public static Assembly ResourcesAssembly { get; } = typeof(RushResource).Assembly;
+    }
+}
